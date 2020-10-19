717f1ac93c02432f8329e666d2533bc860494b99
tterrag <tterrag1098@gmail.com>
2020-10-19 01:30:50 -0400

    Fix launch wrapping: Remove --run argument, invoke main class directly

--- src/main/java/com/lovetropics/installer/LaunchWrapper.java
+++ src/main/java/com/lovetropics/installer/LaunchWrapper.java
@@ -7,7 +7,6 @@ import java.lang.ProcessBuilder.Redirect;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.beust.jcommander.JCommander;
@@ -20,9 +19,6 @@ import com.lovetropics.installer.config.InstallerConfig;
 public class LaunchWrapper {
 
     private static class Arguments {
-
-        @Parameter(names = { "--run" }, description = "Command to execute root installer with")
-        private String runCommand = "-jar ltinstaller.jar";
 
         @Parameter(names = { "--bypass" }, description = "Bypass the wrapper and launch the installer directly")
         private boolean bypass = false;
@@ -58,10 +54,12 @@ public class LaunchWrapper {
         arguments.add(0, "java");
         arguments.add("-cp");
         arguments.add(classpath + ";" + config.forgeInstallerPath);
-        arguments.addAll(Arrays.asList(args.runCommand.split("\\s+")));
+        arguments.add(LaunchWrapper.class.getCanonicalName());
         arguments.add("--bypass");
         arguments.add("--config");
         arguments.add(args.config);
+        
+        System.out.println("Wrapping new launch with arguments: " + arguments);
 
         new ProcessBuilder(arguments).redirectError(Redirect.INHERIT).redirectOutput(Redirect.INHERIT).start().waitFor();
     }
